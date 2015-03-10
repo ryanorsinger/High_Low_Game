9a774f1ecfd8bd96140408d623c3d97190700e4f
ryanorsinger <ryan.orsinger@gmail.com>
2015-03-10 14:23:10 -0500

    First Commit

---
+++ highlow.php
@@ -0,0 +1,7 @@
+<?php
+
+// game picks a random number between 1 and 100.
+// prompts user to guess the number
+// if user's guess is less than the number, it outputs "HIGHER"
+// if user's guess is more than the number, it outputs "LOWER"
+// if a user guesses the number, the game should declare "GOOD GUESS!"
